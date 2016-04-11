581593316b6f5a05e08d60fcbcb63b9ebc541e07
rgbitx <rgbitx@gmail.com>
2016-04-11 22:24:48 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int search(vector<int> &A, int target) {
+    if (A.size() == 0) {
+        return -1;
+    }
+
+    int start = 0, end = A.size() - 1, mid = 0;
+    while (start + 1 < end) {
+        mid = start + (end - start) / 2;
+        if (A[mid] == target) {
+            return mid;
+        }
+        if (A[start] < A[mid]) {
+            if (A[start] <= target && A[mid] > target) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        } else {
+            if (A[mid] < target && target <= A[end]){
+                start = mid;
+            } else {
+                end =start;
+            }
+        }
+
+        if (A[start] == target) {
+            return start;
+        }
+        if (A[end] == target) {
+            return end;
+        }
+        return -1;
+    }
+
+}
+
+
+int main() {
+    cout << "Hello, World!" << endl;
+    vector<int> A = {4, 5, 6, 7, 0, 1, 2};
+    int target = 0;
+
+    int ret = search(A, target);
+
+    cout<<ret;
+
+    return 0;
+}
